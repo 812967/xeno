d13b7082022f2d06966f8d98b7926e14338df36b
Calvin <146762695+812967@users.noreply.github.com>
2024-06-04 10:01:19 -0700

    aded OuterGame/

--- OuterGame/js/loadResources.js
+++ OuterGame/js/loadResources.js
@@ -1,67 +1,67 @@
 let imageSources = {//Properties of imageSources match names of images
 	//outer game images
 	Asteroid1: {
-		sourceString: "res/sprites/Planet/Asteroid1.png",
+		sourceString: "OuterGame/res/sprites/Planet/Asteroid1.png",
 	},
 	Asteroid2: {
-		sourceString: "res/sprites/Planet/Asteroid2.png",
+		sourceString: "OuterGame/res/sprites/Planet/Asteroid2.png",
 	},
 	Asteroid3: {
-		sourceString: "res/sprites/Planet/Asteroid3.png",
+		sourceString: "OuterGame/res/sprites/Planet/Asteroid3.png",
 	},
 	Asteroid4: {
-		sourceString: "res/sprites/Planet/Asteroid4.png",
+		sourceString: "OuterGame/res/sprites/Planet/Asteroid4.png",
 	},
 	Asteroid5: {
-		sourceString: "res/sprites/Planet/Asteroid5.png",
+		sourceString: "OuterGame/res/sprites/Planet/Asteroid5.png",
 	},
 	Asteroid6: {
-		sourceString: "res/sprites/Planet/Asteroid6.png",
+		sourceString: "OuterGame/res/sprites/Planet/Asteroid6.png",
 	},
 	Asteroid7: {
-		sourceString: "res/sprites/Planet/Asteroid7.png",
+		sourceString: "OuterGame/res/sprites/Planet/Asteroid7.png",
 	},
 	Asteroid8: {
-		sourceString: "res/sprites/Planet/Asteroid8.png",
+		sourceString: "OuterGame/res/sprites/Planet/Asteroid8.png",
 	},
 	Asteroid9: {
-		sourceString: "res/sprites/Planet/Asteroid9.png",
+		sourceString: "OuterGame/res/sprites/Planet/Asteroid9.png",
 	},
 	Asteroid10: {
-		sourceString: "res/sprites/Planet/Asteroid10.png",
+		sourceString: "OuterGame/res/sprites/Planet/Asteroid10.png",
 	},
 	Planet1: {
-		sourceString: "res/sprites/Planet/p0001.png",
+		sourceString: "OuterGame/res/sprites/Planet/p0001.png",
 	},
 	Planet2: {
-		sourceString: "res/sprites/Planet/p0010.png",
+		sourceString: "OuterGame/res/sprites/Planet/p0010.png",
 	},
 	Planet3: {
-		sourceString: "res/sprites/Planet/p0011.png",
+		sourceString: "OuterGame/res/sprites/Planet/p0011.png",
 	},
 	Planet4: {
-		sourceString: "res/sprites/Planet/p0008.png",
+		sourceString: "OuterGame/res/sprites/Planet/p0008.png",
 	},
 	Planet5: {
-		sourceString: "res/sprites/Planet/p0006.png",
+		sourceString: "OuterGame/res/sprites/Planet/p0006.png",
 	},
 	playerShip: {
-		sourceString: "res/sprites/Finalimgs/SpaceShip.png",
+		sourceString: "OuterGame/res/sprites/Finalimgs/SpaceShip.png",
 	},
 	starfield: {
-		sourceString: "res/starfield.png"
+		sourceString: "OuterGame/res/starfield.png"
 	},
 	Station: {
-		sourceString: "res/sprites/Finalimgs/AllySpaceStation4.png"
+		sourceString: "OuterGame/res/sprites/Finalimgs/AllySpaceStation4.png"
 	},
 	Drone1: {
-		sourceString: "res/sprites/Finalimgs/Drone2.png"
+		sourceString: "OuterGame/res/sprites/Finalimgs/Drone2.png"
 	},
 	Drone2: {
-		sourceString: "res/sprites/Finalimgs/Drone3.png"
+		sourceString: "OuterGame/res/sprites/Finalimgs/Drone3.png"
 	},
 	Drone3: {
-		sourceString: "res/sprites/Finalimgs/Drone4.png"
+		sourceString: "OuterGame/res/sprites/Finalimgs/Drone4.png"
 	},
 	//inner game images
 	grass: {
@@ -114,41 +114,41 @@ let imageSources = {//Properties of imageSources match names of images
 	},
 	// Panels
 	logohead: {
-		sourceString:"res/panels/header.png",
+		sourceString:"OuterGame/res/panels/header.png",
 	},
 	question: {
-		sourceString:"res/panels/questionMark.png",
+		sourceString:"OuterGame/res/panels/questionMark.png",
 	},
 	splashpanel: {
-		sourceString:"res/panels/TheSplash.png",
+		sourceString:"OuterGame/res/panels/TheSplash.png",
 	},
 	helpinner: {
-		sourceString:"res/panels/panel_IG.png",
+		sourceString:"OuterGame/res/panels/panel_IG.png",
 	},
 	helpouter: {
-		sourceString:"res/panels/panel_OG.png",
+		sourceString:"OuterGame/res/panels/panel_OG.png",
 	},
 	panel00: {
-		sourceString:"res/panels/IntroText2.png",
+		sourceString:"OuterGame/res/panels/IntroText2.png",
 	},
 	panel01: {
-		sourceString:"res/panels/bluedown.png",
+		sourceString:"OuterGame/res/panels/bluedown.png",
 	},
 	panel02: {
-		sourceString:"res/panels/greendown.png",
+		sourceString:"OuterGame/res/panels/greendown.png",
 	},
 	panel03: {
-		sourceString:"res/panels/greydown.png",
+		sourceString:"OuterGame/res/panels/greydown.png",
 	},
 	panel04: {
-		sourceString:"res/panels/purpledown.png",
+		sourceString:"OuterGame/res/panels/purpledown.png",
 	},
 	panel05: {
-		sourceString:"res/panels/reddown.png",
+		sourceString:"OuterGame/res/panels/reddown.png",
 	},
 	// Trading post images
 	stationBackground: {
-		sourceString:"shopIMGS/background.png",
+		sourceString:"OuterGame/shopIMGS/background.png",
 
 
 		roverEngine: {
@@ -207,124 +207,124 @@ function loadImages() {
 	});
 }
 
-// let soundSources = {//Properties of soundSources match names of sounds
-// 	backgroundMusic: {
-// 		sourceString:"res/sounds/A/bgS.mp3",
-// 	},
-// 	backgroundMusicAlt: {
-// 		sourceString:"res/sounds/B/bgS.mp3",
-// 	},
-// 	badNoiseDontUse: {
-// 		sourceString:"res/sounds/B/GoodNoiseplsuse.wav",
-// 	},
-// 	click: {
-// 		sourceString:"res/sounds/A/click.mp3",
-// 	},
-// 	cllsn: {
-// 		sourceString:"res/sounds/A/cllsn.mp3",
-// 	},
-// 	ding: {
-// 		sourceString:"res/sounds/A/ding.mp3",
-// 	},
-// 	doom: {
-// 		sourceString:"res/sounds/A/doom.mp3",
-// 	},
-// 	error: {
-// 		sourceString:"res/sounds/B/err.wav",
-// 	},
-// 	keyStroke1: {
-// 		sourceString:"res/sounds/B/computernoise1.wav",
-// 	},
-// 	keyStroke2: {
-// 		sourceString:"res/sounds/B/computernoise2.wav",
-// 	},
-// 	keyStroke3: {
-// 		sourceString:"res/sounds/B/computernoise3.wav",
-// 	},
-// 	mchn: {
-// 		sourceString:"res/sounds/A/mchn.mp3",
-// 	},
-// 	money: {
-// 		sourceString:"res/sounds/A/money.mp3",
-// 	},
-// 	point: {
-// 		sourceString:"res/sounds/B/points.wav",
-// 	},
-// 	mS1: {
-// 		sourceString:"res/sounds/A/mS1.mp3",
-// 	},
-// 	shipCollide1: {
-// 		sourceString:"res/sounds/B/CollidingwithOtherShips1.wav",
-// 	},
-// 	shipCollide2: {
-// 		sourceString:"res/sounds/B/CollidingwithOtherShips3.wav",
-// 	},
-// 	shipForwards: {
-// 		sourceString:"res/sounds/B/ShipNoiseForward.wav",
-// 	},
-// 	shipLand: {
-// 		sourceString:"res/sounds/B/SpaceShipLanding1.wav",
-// 	},
-// 	smash: {
-// 		sourceString:"res/sounds/A/smash.mp3",
-// 	},
-// 	spaceSounds: {
-// 		sourceString:"res/sounds/B/spaceambience1.wav",
-// 	},
-// 	takeoff: {
-// 		sourceString:"res/sounds/B/Takeoff.wav",
-// 	},
-// 	tDest: {
-// 		sourceString:"res/sounds/A/tDest.mp3",
-// 	},
-// 	tNoise: {
-// 		sourceString:"res/sounds/A/tNoise.mp3",
-// 	},
-// 	tpLand: {
-// 		sourceString:"res/sounds/B/LandingonTradePost.wav",
-// 	},
-// 	tradeErr: {
-// 		sourceString:"res/sounds/B/unsuccessfultrade.wav",
-// 	},
-// 	uPup: {
-// 		sourceString:"res/sounds/A/uPup.mp3",
-// 	},
-// 	water: {
-// 		sourceString:"res/sounds/A/water.mp3",
-// 	},
-// 	weaponPulse: {
-// 		sourceString:"res/sounds/A/weapon.mp3",
-// 	},
-// 	xplsn1: {
-// 		sourceString:"res/sounds/B/explosion4.wav",
-// 	},
-// 	xplsn2: {
-// 		sourceString:"res/sounds/B/explosion5.wav",
-// 	},
-// 	xplsn3: {
-// 		sourceString:"res/sounds/B/explosion3.wav",
-// 	}
-// }
+let soundSources = {//Properties of soundSources match names of sounds
+	backgroundMusic: {
+		sourceString:"OuterGame/res/sounds/A/bgS.mp3",
+	},
+	backgroundMusicAlt: {
+		sourceString:"OuterGame/res/sounds/B/bgS.mp3",
+	},
+	badNoiseDontUse: {
+		sourceString:"OuterGame/res/sounds/B/GoodNoiseplsuse.wav",
+	},
+	click: {
+		sourceString:"OuterGame/res/sounds/A/click.mp3",
+	},
+	cllsn: {
+		sourceString:"OuterGame/res/sounds/A/cllsn.mp3",
+	},
+	ding: {
+		sourceString:"OuterGame/res/sounds/A/ding.mp3",
+	},
+	doom: {
+		sourceString:"OuterGame/res/sounds/A/doom.mp3",
+	},
+	error: {
+		sourceString:"OuterGame/res/sounds/B/err.wav",
+	},
+	keyStroke1: {
+		sourceString:"OuterGame/res/sounds/B/computernoise1.wav",
+	},
+	keyStroke2: {
+		sourceString:"OuterGame/res/sounds/B/computernoise2.wav",
+	},
+	keyStroke3: {
+		sourceString:"OuterGame/res/sounds/B/computernoise3.wav",
+	},
+	mchn: {
+		sourceString:"OuterGame/res/sounds/A/mchn.mp3",
+	},
+	money: {
+		sourceString:"OuterGame/res/sounds/A/money.mp3",
+	},
+	point: {
+		sourceString:"OuterGame/res/sounds/B/points.wav",
+	},
+	mS1: {
+		sourceString:"OuterGame/res/sounds/A/mS1.mp3",
+	},
+	shipCollide1: {
+		sourceString:"OuterGame/res/sounds/B/CollidingwithOtherShips1.wav",
+	},
+	shipCollide2: {
+		sourceString:"OuterGame/res/sounds/B/CollidingwithOtherShips3.wav",
+	},
+	shipForwards: {
+		sourceString:"OuterGame/res/sounds/B/ShipNoiseForward.wav",
+	},
+	shipLand: {
+		sourceString:"OuterGame/res/sounds/B/SpaceShipLanding1.wav",
+	},
+	smash: {
+		sourceString:"OuterGame/res/sounds/A/smash.mp3",
+	},
+	spaceSounds: {
+		sourceString:"OuterGame/res/sounds/B/spaceambience1.wav",
+	},
+	takeoff: {
+		sourceString:"OuterGame/res/sounds/B/Takeoff.wav",
+	},
+	tDest: {
+		sourceString:"OuterGame/res/sounds/A/tDest.mp3",
+	},
+	tNoise: {
+		sourceString:"OuterGame/res/sounds/A/tNoise.mp3",
+	},
+	tpLand: {
+		sourceString:"OuterGame/res/sounds/B/LandingonTradePost.wav",
+	},
+	tradeErr: {
+		sourceString:"OuterGame/res/sounds/B/unsuccessfultrade.wav",
+	},
+	uPup: {
+		sourceString:"OuterGame/res/sounds/A/uPup.mp3",
+	},
+	water: {
+		sourceString:"OuterGame/res/sounds/A/water.mp3",
+	},
+	weaponPulse: {
+		sourceString:"OuterGame/res/sounds/A/weapon.mp3",
+	},
+	xplsn1: {
+		sourceString:"OuterGame/res/sounds/B/explosion4.wav",
+	},
+	xplsn2: {
+		sourceString:"OuterGame/res/sounds/B/explosion5.wav",
+	},
+	xplsn3: {
+		sourceString:"OuterGame/res/sounds/B/explosion3.wav",
+	}
+}
 
 
   function loadSounds(){
 	let promises = [];
-	// for(let index in soundSources){
-	// 	if(soundSources.hasOwnProperty(index)){
-	// 		promises.push(new Promise(function(resolve,reject){
-	// 			var sourceString = soundSources[index].sourceString;
-	// 			var sou = new Audio(sourceString);
-	// 			sou.addEventListener('load', function(){
-	// 				resolve(img);
-	// 			});
-	// 			sou.addEventListener('error', function(){
-	// 				reject();
-	// 			});
-	// 		}).then(function(img){
-	// 			Sounds[index] = sou;
-	// 		}))
-	// 	}
-	// }
+	for(let index in soundSources){
+		if(soundSources.hasOwnProperty(index)){
+			promises.push(new Promise(function(resolve,reject){
+				var sourceString = soundSources[index].sourceString;
+				var sou = new Audio(sourceString);
+				sou.addEventListener('load', function(){
+					resolve(img);
+				});
+				sou.addEventListener('error', function(){
+					reject();
+				});
+			}).then(function(img){
+				Sounds[index] = sou;
+			}))
+		}
+	}
 	Promise.all(promises).then(function(){
 		init();
 	});
